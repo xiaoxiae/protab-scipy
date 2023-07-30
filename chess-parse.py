f961dc5e859565a220dcdfdce6115b290663987d
xiaoxiae <tomas@slama.dev>
2023-07-30 18:10:45 +0200

    Šachy v2

--- chess-parse.py
+++ chess-parse.py
@@ -1,39 +1,52 @@
 import json
 from datetime import datetime
 
-with open("tmp.txt") as f:
+with open("lichess_db_standard_rated_2013-01.txt") as f:
     lines = f.read().split("\n\n")
+
 
 games = {}
 
-for i in range(0, 1000, 2):
+for i in range(0, 10000, 2):
     game = {}
 
     # moves
     move_parts = lines[i + 1].split()
+
+    # some games are just weird (have evals)
+    if "{" in move_parts:
+        continue
+
     moves = []
     for j in range(1, len(move_parts), 3):
         moves.append(move_parts[j])
         moves.append(move_parts[j + 1])
 
     # metadata
+    game_id = None
     for l in lines[i].splitlines():
+        value = l.split()[1][1:-2]
+
         if l.startswith("[WhiteElo"):
-            game["white_rating"] = l.split()[1][1:-2]
+            game["white_rating"] = value
 
         if l.startswith("[BlackElo"):
-            game["black_rating"] = l.split()[1][1:-2]
+            game["black_rating"] = value
 
         if l.startswith("[UTCDate"):
-            game["date"] = l.split()[1][1:-2]
+            game["date"] = value
 
+        if l.startswith("[Site"):
+            game_id = value.split("/")[-1]
+
+    if game_id is None:
+        continue
 
     game["result"] = move_parts[-1]
     game["moves"] = moves
 
-    games[i // 2 + 1] = game
+    games[game_id] = game
 
-
-with open("out.json", "w") as f:
+with open("data/sachy.json", "w") as f:
     f.write(json.dumps(games))
 
